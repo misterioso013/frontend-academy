53ca65ed9b14fae8333d28c8c38fe5df1445eb62
Rosiel Victor <rosielvictor.dev@gmail.com>
2024-10-12 13:45:46 -0300

    feat: Adicionando tema

---
+++ src/app/editor/web/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "../../globals.css";
+
+const geistSans = localFont({
+  src: "../../fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "../../fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+    title: "Front-end Editor - Front-end Academy",
+    description: "Editor de código front-end para testar códigos HTML, CSS e JavaScript em tempo real.",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
